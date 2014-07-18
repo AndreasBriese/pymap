d435c4d79aa7794074e5bf6ec70d5d93fdf20be0
AndreasBriese <ab@edutoolbox.de>
2014-07-18 20:37:49 +0200

    function comments added

--- pymap.go
+++ pymap.go
@@ -11,6 +11,10 @@ func init() {
 }
 
 func sumIntArray(a *[]int) (s int) {
+	`sumIntArray 
+	takes a pointer to an array/slice of type []int 
+	(will be int32/int64 depending on OS & compiler settings)
+	returns the sum of the values`
 	for _, v := range *a {
 		s += v
 	}
@@ -18,6 +22,10 @@ func sumIntArray(a *[]int) (s int) {
 }
 
 func sumUintArray(a *[]uint) (s uint) {
+	`sumUintArray 
+	takes a pointer to an array/slice of type []uint 
+	(will be uint32/uint64 depending on OS & compiler settings)
+	returns the sum of the values`
 	for _, v := range *a {
 		s += v
 	}
@@ -25,6 +33,10 @@ func sumUintArray(a *[]uint) (s uint) {
 }
 
 func sumInt64Array(a *[]int64) (s int64) {
+	`sumInt64Array 
+	takes a pointer to an array/slice of type []int64
+	(allows casting of int64 values if int was compiled as int32)
+	returns the sum of the values`
 	for _, v := range *a {
 		s += v
 	}
@@ -32,6 +44,10 @@ func sumInt64Array(a *[]int64) (s int64) {
 }
 
 func sumUint64Array(a *[]uint64) (s uint64) {
+	`sumUint64Array 
+	takes a pointer to an array/slice of type []uint64
+	(allows casting of uint64 values if uint was compiled as uint32)
+	returns the sum of the values`
 	for _, v := range *a {
 		s += v
 	}
@@ -39,6 +55,16 @@ func sumUint64Array(a *[]uint64) (s uint64) {
 }
 
 func Each(a *[]int, fn func(*[]int, int, int, int)) time.Duration {
+	`Each 
+	takes 
+		a pointer to an array/slice 'a' of type []int
+		a func to process each value in the slice/array 'a'
+			func takes 
+				a pointer to 'a'
+				an index 'i' 
+				two ints to be used within func
+					i.e. a[i] = x */-+ y
+	returns the time.Duration of processing`
 	st := time.Now()
 	for i, v := range *a {
 		fn(a, i, v, v)
