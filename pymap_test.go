8950341f31b73de0c95ed6c597a3e7f2d96fbbfa
AndreasBriese <ab@edutoolbox.de>
2014-07-18 22:48:54 +0200

    more typos

--- pymap_test.go
+++ pymap_test.go
@@ -1,14 +1,9 @@
 package pymap
 
 import (
-	"fmt"
+	//  "fmt"
 	. "github.com/smartystreets/goconvey/convey"
 	"testing"
-	"time"
-)
-
-var (
-	t1, t2 time.Duration
 )
 
 func TestSumme_IntArray(t *testing.T) {
@@ -114,22 +109,21 @@ func TestEqualArrays(t *testing.T) {
 
 func TestEach(t *testing.T) {
 
-	Convey("Given a []int {1,2,3}", t, func() {
+	Convey("Given a []int {0...999}", t, func() {
 		a := []int{}
-		for i := 0; i < 100000000; i++ {
+		for i := 0; i < 1000; i++ {
 			a = append(a, i)
 		}
 
 		Convey("Each x*x", func() {
-			t1 = Each(&a, func(a *[]int, i, x, y int) {
+			Each(&a, func(a *[]int, i, x, y int) {
 				(*a)[i] = x * y
 			})
 
 			Convey("Now a should be 1, 4, 9 ", nil)
-			fmt.Println(t1)
-			// So(a[0], ShouldEqual, 1)
-			// So(a[1], ShouldEqual, 4)
-			// So(a[2], ShouldEqual, 9)
+			So(a[1], ShouldEqual, 1)
+			So(a[2], ShouldEqual, 4)
+			So(a[3], ShouldEqual, 9)
 		})
 
 	})
@@ -150,24 +144,12 @@ func TestEachNew(t *testing.T) {
 			})
 
 			Convey("Now b should be Each(a, fn=x*x) ", nil)
-			t1 = Each(&a, func(a *[]int, i, x, y int) {
+			Each(&a, func(a *[]int, i, x, y int) {
 				(*a)[i] = x * y
 			})
 
 			So(EqualArrays(&b, &a), ShouldBeTrue)
-			// type bi interface {
-			// 	Len
-			// }
-			// bii := bi(b)
-			// switch i := bi.(type) {
-			// case int:
-			// 	fmt.Println(i)
-			// default:
-			// 	fmt.Println(i)
-			// }
 
-			// So(a[1], ShouldEqual, 4)
-			// So(a[2], ShouldEqual, 9)
 		})
 
 	})
@@ -184,20 +166,17 @@ func TestEach2Seq(t *testing.T) {
 		b := make([]int, len(a))
 		copy(b, a)
 
-		Convey("Each s1[i]*s2[i]", func() {
-			t2 = Each2Seq(&b, &a, func(s1, s2 *[]int, i int) {
+		Convey("Each2Seq s1[i]*s2[i]", func() {
+			Each2Seq(&b, &a, func(s1, s2 *[]int, i int) {
 				(*s1)[i] *= (*s2)[i]
 			})
-			t1 = Each(&a, func(a *[]int, i, x, y int) {
+			Each(&a, func(a *[]int, i, x, y int) {
 				(*a)[i] = x * y
 			})
 
-			Convey("Now a should be 2, 8, 18 ", nil)
-			fmt.Println(t2, t1 < t2)
-			fmt.Println(EqualArrays(&a, &b))
-			// So(a[0], ShouldEqual, 2)
-			// So(a[1], ShouldEqual, 8)
-			// So(a[2], ShouldEqual, 18)
+			Convey("Now b should equal a**2", nil)
+			So(EqualArrays(&a, &b), ShouldBeTrue)
+
 		})
 
 	})
